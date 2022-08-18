c6b4533153cb9265e1ebb256a7424b95280d9067
nikeshDahal <nikeshdahal297@gmail.com>
2022-08-18 15:09:14 +0545

    CRUD operation finished with mongoDB

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,38 @@
-import { Body,Controller , Post} from '@nestjs/common';
+import { Body,Controller , Post , Get , Param, Delete , Patch , NotFoundException} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
     constructor( private usersService : UsersService){}
     @Post('/signup')
-    createUser( @Body() body:CreateUserDto){
-        const user = this.usersService.createUser(body.username, body.email, body.password);
+    async createUser( @Body() body:CreateUserDto){
+        const user = await this.usersService.createUser(body.username, body.email, body.password);
         return user
+    }
+
+    @Get('/:id')
+    async findUser(@Param('id') id:string){
+        const user = await this.usersService.findOne((id));
+        if(!user){
+            throw new NotFoundException('user not found')
+        }
+        return user;
+    }
+
+    @Get()
+    async findAllUsers(){
+       return await this.usersService.findAll();
+    }
+
+    @Delete('/:id')
+    async removeUser(@Param('id') id : string){
+        return await this.usersService.deleteUser((id));
+    }
+
+    @Patch('/:id')
+    async updateUser(@Param('id') id:string , @Body() body: UpdateUserDto ){
+        return await this.usersService.updateUser(id,body)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import {InjectModel} from '@nestjs/mongoose'
-// import {Repository} 
-import {Model} from 'mongoose'
-import {User} from './user.model'
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+// import {Repository}
+import { Model } from 'mongoose';
+import { User } from './user.model';
+var mongoose = require('mongoose');
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel('User') private userModel :Model<User>){}
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
 
-    async createUser(username:string, email:string , password : string){
-        const newUser = new this.userModel({
-            username,
-            email,
-            password
-        });
-        const user = await newUser.save();
-        console.log(user)
-        return user;
+  async createUser(username: string, email: string, password: string) {
+    const newUser = new this.userModel({
+      username,
+      email,
+      password,
+    });
+    const user = await newUser.save();
+    return user;
+  }
+
+  async findOne(id: string) {
+      return await this.userModel.findOne({ _id: id });
+  }
+
+  async findAll() {
+      return await this.userModel.find();
+  }
+
+  async updateUser(id: string, attributes: Partial<User>) {
+    const user = await this.userModel.findOne({ _id: id });
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+    const updatedUser = Object.assign(user, attributes);
+    const userAfterSavingInDatabase = await updatedUser.save();
+    return userAfterSavingInDatabase;
+  }
+
+  async deleteUser(id: string) {
+    const user = await this.userModel.findOne({ _id: id });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    await this.userModel.deleteOne({ _id: id }).exec();
+    return user;
+  }
 }
