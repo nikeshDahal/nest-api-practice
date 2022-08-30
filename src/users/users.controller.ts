4875b7c19202d9e3b0791a575177f26656a52a16
nikeshDahal <nikeshdahal297@gmail.com>
2022-08-30 14:17:19 +0545

    current user added in all routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,38 +1,28 @@
 import {
   Body,
   Controller,
-  Post,
   Get,
-  Param,
   Delete,
   Patch,
   NotFoundException,
   UseGuards,
-  Request
 } from '@nestjs/common';
 
-// import { CreateUserDto } from '../auth/dto/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { currentUser } from 'src/auth/currentUser';
-// import { AuthService } from 'src/auth/auth.service';
-
 
 @Controller('auth')
 @serialize(UserDto)
 @UseGuards(AuthGuard('jwt'))
 export class UsersController {
-  constructor(
-    private usersService: UsersService,
-    // private authService: AuthService,
-  ) {}
+  constructor(private usersService: UsersService) {}
 
-
-  @Get()
-  async getMyProfile(@currentUser() req:any) {
+  @Get('/profile')
+  async getMyProfile(@currentUser() req: any) {
     const user = await this.usersService.findOne(req._id);
     if (!user) {
       throw new NotFoundException('user not found');
@@ -40,19 +30,22 @@ export class UsersController {
     return user;
   }
 
-  @Get()
-  async findAllUsers(@Request() req:any) {
-    console.log('find all users :', req.user)
+  @Get('/listUsers')
+  async findAllUsers() {
     return await this.usersService.findAll();
   }
 
-  @Delete('/:id')
-  async removeUser(@Param('id') id: string) {
-    return await this.usersService.deleteUser(id);
+  @Delete('/delete')
+  async removeUser(@currentUser() req: any) {
+    return await this.usersService.deleteUser(req._id);
   }
 
-  @Patch('/:id')
-  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    return await this.usersService.updateUser(id, body);
+  @Patch('/update')
+  async updateUser(@currentUser() req: any, @Body() body: UpdateUserDto) {
+   const updatedUser = await this.usersService.updateUser(req._id, body);
+   console.log(updatedUser);
+   req=updatedUser
+   console.log('request  after update:', req)
+   return req;
   }
 }
